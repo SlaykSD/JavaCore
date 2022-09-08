a6d6119f4e81ac8d6aa24d04d7128dfbcbb07f99
SlaykSD <60920077+SlaykSD@users.noreply.github.com>
2022-09-08 15:28:08 +0300

    add task2

--- src/main/java/ru/ylab/task2/Main.java
+++ src/main/java/ru/ylab/task2/Main.java
@@ -1,42 +1,116 @@
 package ru.ylab.task2;
 
+  /*
+        Task2
+
+            [3, 4, 2, 7], 10 -> [3, 7] - вывести пару именно в скобках, которые дают сумму - 10
+         */
+
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        int [] arr = {5,6,3,2,5,1,4,9};
-        sortShell(arr);
-        show(arr);
-    }
-    public static  void  show(int [] arr){
-        StringBuilder msg = new StringBuilder("Answer: \n");
-        msg.append(" {");
-        for (int i : arr) {
-            msg.append(" [").append(i).append("]");
+    static final Integer [] RAW = {3,4,2,7};
+
+    /**
+     * The function searches for two elements of an unsorted
+     * array whose sum is equal to a given number.
+     * The algorithm is executed for asymptotic complexity  O(nlogn) + O(n)
+     * O(nlogn) - its Heapsort
+     * O(n) - find sum
+     *
+     * @param mas - array of Integer
+     * @param specialNumber - a given sum of two numbers of the array
+     * @return an array of two numbers  OR null (if not found the pair)
+     */
+    public static Integer[] findSpecialPair(Integer[] mas, Integer specialNumber){
+        heapSort(mas);//nlogn sorting
+
+        int lt = 0;
+        int rt = mas.length-1;
+        while (lt != rt){
+            int curr = mas[lt]+ mas[rt];
+            if(curr< specialNumber){
+                lt++;
+            }
+            else if(curr> specialNumber){
+                rt--;
+            }else {
+                return new Integer[] {mas[lt],mas[rt]};
+            }
         }
-        msg.append(" }\n");
-        System.out.println(msg.toString());
+        return null;
     }
 
     /**
-     * The usual sorting by class is shell sorting.
-     * It is slower than fast sorting, but requires less memory.
-     * It is more efficient on small data sets.
-     *
-     * @param array - array of integers
+     * Main function for heapsort
+     * @param a - array to be sorted
      */
-    public static void sortShell(int[] array) {
-        int n = array.length;
-
-        for (int h = n / 2; h > 0; h /= 2) {
-            for (int i = h; i < n; i++) {
-                int key = array[i];
-                int j = i;
-                while (j >= h && array[j - h] > key) {
-                    array[j] = array[j - h];
-                    j -= h;
-                }
-                array[j] = key;
-            }
+    public static  void heapSort(Integer[] a) {
+        heapify(a);
+        var end = a.length - 1;
+        while (end > 0) {
+            var temp = a[end];
+            a[end] = a[0];
+            a[0] = temp;
+            end--;
+            siftDown(a, 0, end);
         }
     }
 
+    private static void heapify(Integer[] a) {
+        var start = (a.length - 2) / 2;
+        while (start >= 0) {
+            siftDown(a, start, a.length - 1);
+            start--;
+        }
+    }
+
+    private static  void  siftDown(Integer[] a,Integer start,Integer end) {
+        var root = start;
+        while (root * 2 + 1 <= end) {
+            var child = root * 2 + 1;
+            if (child + 1 <= end && a[child] < a[child + 1]) child++;
+            if (a[root] < a[child]) {
+                var temp = a[root];
+                a[root] = a[child];
+                a[child] = temp;
+                root = child;
+            }
+            else return;
+        }
+    }
+
+    /**
+     * Show the result
+     */
+    public  static void show(Integer[] data, Integer[] result, Integer sum){
+        System.out.print("[");
+        for (int i = 0; i< data.length-1;i++){
+            System.out.print(data[i]+", ");
+        }
+        System.out.print(data[data.length-1]+"], "+sum+" -> ");
+        if(result!=null) {
+            System.out.print("[" + result[0] + ", " + result[1] + "]");
+        }else{
+            System.out.println("Not found");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        var arr = findSpecialPair(RAW,10);
+        show(RAW,arr,10);
+
+        //For test
+        Integer[] dataSet1 = {1,10,16,29,-10,20,30,100}; //find 10
+        arr = findSpecialPair(dataSet1,10);
+        show(dataSet1,arr,10);
+
+        arr = findSpecialPair(dataSet1,11);
+        show(dataSet1,arr,11);
+
+        arr = findSpecialPair(dataSet1,12);
+        show(dataSet1,arr,12);
+    }
 }
