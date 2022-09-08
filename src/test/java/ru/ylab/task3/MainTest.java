0a72e46513e5d17c60b23e5ddb72ff4651d7adad
SlaykSD <60920077+SlaykSD@users.noreply.github.com>
2022-09-08 14:29:26 +0300

    add task3

---
+++ src/test/java/ru/ylab/task3/MainTest.java
@@ -0,0 +1,20 @@
+package ru.ylab.task3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.ylab.task3.Main.fuzzySearch;
+
+class MainTest {
+
+    @Test
+    void fuzzySearchTest() {
+        String dataSearchSet1 = "jdbc";
+        String dataVerifiable = "   nsetJjjjasjdbodc";
+        assertTrue(fuzzySearch(dataSearchSet1, dataVerifiable));
+        dataVerifiable = "   nsetJjjjasjdbod       c";
+        assertTrue(fuzzySearch(dataSearchSet1, dataVerifiable));
+        dataVerifiable = "   nsetJjjjasjdbod       ";
+        assertFalse(fuzzySearch(dataSearchSet1, dataVerifiable));
+    }
+}
